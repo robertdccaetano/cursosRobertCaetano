69f076e2ffe6d113d899316680f055521c40b642
robertdccaetano <robertvisualmidia@gmail.com>
2018-12-26 16:43:53 -0200

    Subindo Desafio #008 v1.1 [26/12/2018]

--- PythonDesafios/ex008.py
+++ PythonDesafios/ex008.py
@@ -1,3 +1,5 @@
 m = float(input("Digite o valor em metros: "))
-cm = m/0.010000
-print("{:.0f} Metro(s) convertido em Centímetros é {:.0f}cm ".format(m, cm))
+#cm = m/0.010000
+cm = m*100
+mm = cm*10
+print("{:.0f} Metro(s) convertido em Centímetros é {:.0f}cm e para Milímetros é {:.0f}mm ".format(m, cm, mm))
