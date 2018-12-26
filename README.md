0f4615df5a6f10da261ad67bb648952e2f446f98
robertdccaetano <robertvisualmidia@gmail.com>
2018-12-26 15:06:59 -0200

    Reconfigurei o projeto e o interpretador do Python

--- README.md
+++
@@ -1,2 +0,0 @@
-# Curso em Vídeo -- Curso de Python Mundo01
-#### Aqui encontra-se todos os códigos de desafios e aulas, desenvolvidos no decorrer do curso, sinta-se a vontade para contribuir!
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Robert Caetano\PycharmProjects\Cursoemvideo\venv"
+$env:VIRTUAL_ENV="C:\Users\Robert Caetano\PycharmProjects\untitled\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
