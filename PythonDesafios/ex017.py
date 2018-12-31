eb68f40401562a8cfe9b3324c905993d89ef5c39
robertdccaetano <robertvisualmidia@gmail.com>
2018-12-31 13:04:43 -0200

    Subindo ex017 a 018 31/12/2018

--- PythonDesafios/ex017.py
+++ PythonDesafios/ex017.py
@@ -1,5 +1,5 @@
 import math
 catoposto = float(input('Digite o valor do cateto oposto: '))
-catadcente = float(input('Digite o valor do cateto adjacente: '))
-hipo = ((catoposto ** 2) + (catadcente ** 2))
+catadjacente = float(input('Digite o valor do cateto adjacente: '))
+hipo = ((catoposto ** 2) + (catadjacente ** 2))
 print('O quadrado da hipotenusa é {:.2f}'.format(math.sqrt(hipo)))
---
+++ PythonDesafios/ex018.py
@@ -0,0 +1,5 @@
+import math
+ang = float(input('Entre com o valor do ângulo: '))
+print('O valor do cosseno desse ângulo é {}'.format(math.acos(ang)))
+print('O valor do seno desse ângulo é {}'.format(math.sin(ang)))
+print('O valor da tangente desse ângulo é {}'.format(math.tan(ang)))
