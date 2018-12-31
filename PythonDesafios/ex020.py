32fabad84ec86bc3e0f97f8e19932095dc3058fa
robertdccaetano <robertvisualmidia@gmail.com>
2018-12-31 17:49:42 -0200

    Subindo ex020 ordem do random alunos

--- PythonDesafios/ex020.py
+++ PythonDesafios/ex020.py
@@ -1,4 +1,8 @@
+import random
+
 aluno1 = input('Digite o nome do primeiro aluno 1º ')
 aluno2 = input('Digite o nome do segundo  aluno 2º ')
 aluno3 = input('Digite o nome do terceiro aluno 3º ')
 aluno4 = input('Digite o nome do quarto   aluno 4º ')
+
+print('A ordem de sorteados para apresentação  é  {} '.format(random.sample([aluno1, aluno2, aluno3, aluno4], k=4)))
