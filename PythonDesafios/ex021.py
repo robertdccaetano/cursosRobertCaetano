4a8702e8fee63d129a0318fa60c244fa5f685ed9
robertdccaetano <robertvisualmidia@gmail.com>
2019-01-02 20:05:55 -0200

    Tocando MP# ex021 + mp3

--- PythonDesafios/ex021.py
+++ PythonDesafios/ex021.py
@@ -1,2 +1,8 @@
-import musicview
+import playsound
+playsound.playsound('ex021.mp3')
 
+#from pygame import mixer
+#mixer.init()
+#mixer.music.load('ex021.mp3')
+#mixer.music.play()
+#x = input('Digite algo para parar!')
