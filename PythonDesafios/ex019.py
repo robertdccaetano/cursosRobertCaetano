08e8910102278617863748187b12106131563469
robertdccaetano <robertvisualmidia@gmail.com>
2019-01-03 21:44:06 -0200

    sbindo att do ex019 a 021 corrigidos

--- PythonDesafios/ex019.py
+++ PythonDesafios/ex019.py
@@ -1,3 +1,11 @@
-import random
+from random import choice
 
-print('Prof o aluno(a) que irá apagar o quadro hoje é {}'.format(random.choice(['Pedro', 'Andrea', 'Márcio', 'Franciele'])))
+#print('Prof o aluno(a) que irá apagar o quadro hoje é {}'.format(random.choice(['Pedro', 'Andrea', 'Márcio', 'Franciele'])))
+
+n1 = str(input('Primeiro aluno:  '))
+n2 = str(input('Segundo  aluno:  '))
+n3 = str(input('Terceiro aluno:  '))
+n4 = str(input('Quarto   aluno:  '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
--- PythonDesafios/ex021.py
+++ PythonDesafios/ex021.py
@@ -1,8 +1,8 @@
-import playsound
-playsound.playsound('ex021.mp3')
+'''import playsound
+playsound.playsound('ex021.mp3')'''
 
-#from pygame import mixer
-#mixer.init()
-#mixer.music.load('ex021.mp3')
-#mixer.music.play()
-#x = input('Digite algo para parar!')
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+x = input('Digite algo para parar!')
