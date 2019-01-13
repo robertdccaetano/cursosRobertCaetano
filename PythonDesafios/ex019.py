0f08d89ced779ec807b179c719ccfb94044f8b8e
robertdccaetano <robertvisualmidia@gmail.com>
2019-01-13 16:16:06 -0200

    Estou de volta upando ex019 DATA 13/01/2019

--- PythonDesafios/ex019.py
+++ PythonDesafios/ex019.py
@@ -1,3 +1,16 @@
-import random
+from random import choice
 
-print('Prof o aluno(a) que irá apagar o quadro hoje é {}'.format(random.choice(['Pedro', 'Andrea', 'Márcio', 'Franciele'])))
+# Meu modo de fazer
+'''print('Prof o aluno(a) que irá apagar o quadro hoje é {}'.format(random.choice(['Pedro', 'Andrea', 'Márcio', 
+'Franciele']))) '''
+
+
+# Modo de correção pelo prof
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo  aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto   aluno: '))
+
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido para apagar o quadro é {}'.format(escolhido))
