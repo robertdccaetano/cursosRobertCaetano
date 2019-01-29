faba1065b3fc26948969c7830056abf576284476
robertdccaetano <robertvisualmidia@gmail.com>
2019-01-29 11:21:54 -0200

    Subindo ex023 a 026 já corrigidos

---
+++ Python/PythonDesafios/ex024.py
@@ -0,0 +1,7 @@
+# Meu código
+nomeCity = str(input('Em que cidade você nasceu? ')).strip()
+print('Santo' in nomeCity.capitalize())
+
+# Código de correção
+#cid = str(input('Que cidade voce nasceu? ')).strip()
+#print(cid[0:5].upper() == 'SANTO')
