6745619f444b7438595aa3d88476c5cdcf34484a
robertdccaetano <robertvisualmidia@gmail.com>
2019-03-03 14:24:10 -0300

    Up att ex035

--- Python/PythonDesafios/ex035.py
+++ Python/PythonDesafios/ex035.py
@@ -1,4 +1,5 @@
 from math import fabs
+from time import sleep
 r1 = float(input('Entre com o valor da 1 reta: '))
 r2 = float(input('Entre com o valor da 2 reta: '))
 r3 = float(input('Entre com o valor da 3 reta: '))
@@ -6,7 +7,9 @@ r3 = float(input('Entre com o valor da 3 reta: '))
 if fabs(r2 - r3) < r1 < r2 + r3:
     if fabs(r1 - r3) < r2 < r1 + r3:
         if fabs(r1 - r2) < r3 < r1 + r2:
-            print('É possível formar um triangulo através desses valores!')
+            print('Processando possibilidade...')
+            sleep(3)
+            print('É possível formar um triângulo através desses valores!')
 else:
     print('Não é possível formar um triângulo!')
 
