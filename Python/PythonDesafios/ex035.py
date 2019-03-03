5e86b77ef88b329fe0358cc487738e0af1b01855
robertdccaetano <robertvisualmidia@gmail.com>
2019-03-03 14:01:01 -0300

    Up att ex035

--- Python/PythonDesafios/ex035.py
+++ Python/PythonDesafios/ex035.py
@@ -1,5 +1,14 @@
+from math import fabs
 r1 = float(input('Entre com o valor da 1 reta: '))
 r2 = float(input('Entre com o valor da 2 reta: '))
 r3 = float(input('Entre com o valor da 3 reta: '))
 
-if r2 - r3 %
+if fabs(r2 - r3) < r1 < r2 + r3:
+    if fabs(r1 - r3) < r2 < r1 + r3:
+        if fabs(r1 - r2) < r3 < r1 + r2:
+            print('É possível formar um triangulo através desses valores!')
+else:
+    print('Não é possível formar um triângulo!')
+
+'''result = 8 - 10
+print('Resultado é {:.0f}'.format(fabs(result)))'''
