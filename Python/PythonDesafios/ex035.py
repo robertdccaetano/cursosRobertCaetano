64c74cc1fda32f1946ef5a559a30bdfccebb942b
robertdccaetano <robertvisualmidia@gmail.com>
2019-03-05 19:48:12 -0300

    Att dir

--- Python/PythonDesafios/ex035.py
+++ Python/PythonDesafios/ex035.py
@@ -14,7 +14,7 @@ if fabs(r2 - r3) < r1 < r2 + r3:
             sleep(3)
             print('É POSSÍVEL formar um triângulo através desses valores!')
 else:
-    print('\033[31mNÃO É POSSÍVEL formar um triângulo!\033[m')
+    print('\033[31mNÃO É POSSÍVEL formar um triângulo!!\033[m')
 
 #Solução do vídeo de correção do curso em vídeo
 #if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2
