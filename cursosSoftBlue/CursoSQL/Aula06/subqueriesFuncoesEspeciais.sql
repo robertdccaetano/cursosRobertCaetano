322c13f5c571b3de7b1019ad9a9a3f8ec723587b
robertdccaetano <robertvisualmidia@gmail.com>
2019-09-01 15:54:44 -0300

    Up aula 6.1 cursosql/subqueriesFuncoesEspeciais

---
+++ cursosSoftBlue/CursoSQL/Aula06/subqueriesFuncoesEspeciais.sql
@@ -0,0 +1,39 @@
+USE softblue_cursosql_aulas;
+SELECT * FROM tb_funcionarios;
+SELECT COUNT(*) FROM tb_funcionarios;
+SELECT COUNT(*) FROM tb_funcionarios WHERE salario > 1600;
+SELECT COUNT(*) FROM tb_funcionarios WHERE salario > 1600 AND departamento = 'Jurídico';
+
+SELECT * FROM tb_funcionarios WHERE salario > 1600 AND departamento = 'Jurídico';
+
+SELECT SUM(salario) FROM tb_funcionarios;
+SELECT SUM(salario) FROM tb_funcionarios WHERE departamento = 'TI';
+
+SELECT AVG(salario) FROM tb_funcionarios;
+SELECT AVG(salario) FROM tb_funcionarios WHERE departamento = 'TI';
+
+SELECT MAX(salario) FROM tb_funcionarios;
+SELECT MAX(salario) FROM tb_funcionarios WHERE departamento = 'TI';
+
+SELECT MIN(salario) FROM tb_funcionarios;
+SELECT MIN(salario) FROM tb_funcionarios WHERE departamento = 'TI';
+
+SELECT departamento FROM tb_funcionarios;
+SELECT DISTINCT(departamento) FROM tb_funcionarios;
+
+SELECT * FROM tb_funcionarios;
+SELECT * FROM tb_funcionarios ORDER BY nome;
+SELECT * FROM tb_funcionarios ORDER BY nome DESC;
+
+SELECT * FROM tb_funcionarios ORDER BY salario;
+SELECT * FROM tb_funcionarios ORDER BY departamento;
+SELECT * FROM tb_funcionarios ORDER BY departamento DESC, salario DESC;
+
+SELECT * FROM tb_funcionarios;
+SELECT * FROM tb_funcionarios LIMIT 2;
+
+SELECT * FROM tb_funcionarios LIMIT 2 OFFSET 1;
+SELECT * FROM tb_funcionarios LIMIT 1, 2;
+
+
+
