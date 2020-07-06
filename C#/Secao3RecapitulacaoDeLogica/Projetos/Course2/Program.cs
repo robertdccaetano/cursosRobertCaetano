f5813408d48bace8b12f347d4a8370a02b8b612b
Robert Caetano <robertvisualmidia@gmail.com>
2020-07-05 22:12:23 -0300

    Finalizando tipos de variáveis e convenções de nomes

--- C#/Secao3RecapitulacaoDeLogica/Projetos/Course2/Program.cs
+++ C#/Secao3RecapitulacaoDeLogica/Projetos/Course2/Program.cs
@@ -7,13 +7,16 @@ namespace Course2
         static void Main(string[] args)
         {
             //Valores max e min
-
             int valMin = int.MinValue;
-
             int valMax = int.MaxValue;
+
+            sbyte valSbyteMinValue = sbyte.MinValue;
+            decimal valMaxDecimal = decimal.MaxValue;
 
             Console.WriteLine(valMin);
             Console.WriteLine(valMax);
+            Console.WriteLine(valSbyteMinValue);
+            Console.WriteLine(valMaxDecimal);
         }
     }
 }
