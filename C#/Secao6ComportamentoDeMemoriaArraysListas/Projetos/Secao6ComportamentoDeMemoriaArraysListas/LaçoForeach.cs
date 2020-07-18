10073ed33e18d9de6bebcc81d02cbd8f45dc4864
Robert Caetano <robertvisualmidia@gmail.com>
2020-07-18 13:49:24 -0300

    Estrutura repetição Foreach

---
+++ C#/Secao6ComportamentoDeMemoriaArraysListas/Projetos/Secao6ComportamentoDeMemoriaArraysListas/LaçoForeach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao6ComportamentoDeMemoriaArraysListas
+{
+    class LaçoForeach
+    {
+        public static void Main(string[] args)
+        {
+            string[] vector = new string[] { "Maria", "Marcos", "Lucas" };
+
+            //For convencional
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+            Console.WriteLine("========================");
+
+            foreach (string element in vector)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
--- C#/Secao6ComportamentoDeMemoriaArraysListas/Projetos/Secao6ComportamentoDeMemoriaArraysListas/ModificadorParametrosParams.cs
+++ C#/Secao6ComportamentoDeMemoriaArraysListas/Projetos/Secao6ComportamentoDeMemoriaArraysListas/ModificadorParametrosParams.cs
@@ -6,42 +6,42 @@ namespace Secao6ComportamentoDeMemoriaArraysListas
 {
     class ModificadorParametrosParams
     {
-        public static void Main(string[] args)
-        {
-            //Instancia vetor de forma compacta e já insere os valores nele
-            //com o params na função Sum() dispensa a necessidad de instanciar o vetor na chamada da funcao
-            //int s1 = Calculator.Sum( 2, 3 );
-            //int s2 = Calculator.Sum( 2, 4, 3 );
+        //public static void Main(string[] args)
+        //{
+        //    //Instancia vetor de forma compacta e já insere os valores nele
+        //    //com o params na função Sum() dispensa a necessidad de instanciar o vetor na chamada da funcao
+        //    //int s1 = Calculator.Sum( 2, 3 );
+        //    //int s2 = Calculator.Sum( 2, 4, 3 );
 
-            //Console.WriteLine(s1);
-            //Console.WriteLine(s2);
+        //    //Console.WriteLine(s1);
+        //    //Console.WriteLine(s2);
 
-            int a = 10;
-            //palavra ref no parâmetro passa o valor da variável como referência
-            // A palavra ref exige que a variável seja iniciada Ex: int a = 10;
+        //    int a = 10;
+        //    //palavra ref no parâmetro passa o valor da variável como referência
+        //    // A palavra ref exige que a variável seja iniciada Ex: int a = 10;
 
-            //Passagem por referencia afeta diretamente a variável com base
-            //na posição da memória que ela se encontra
+        //    //Passagem por referencia afeta diretamente a variável com base
+        //    //na posição da memória que ela se encontra
 
-            //Passagem por valor é transferido apenas o valor da variável em questão
-            //para o parâmetro da função chamada
+        //    //Passagem por valor é transferido apenas o valor da variável em questão
+        //    //para o parâmetro da função chamada
 
-            //Variaveis do tipo primitivo por padrão passam por valor
-            //Objetos complexos passão por padrão via referencia à chamada do método
+        //    //Variaveis do tipo primitivo por padrão passam por valor
+        //    //Objetos complexos passão por padrão via referencia à chamada do método
 
 
-            Calculator.Triple(ref a);
+        //    Calculator.Triple(ref a);
 
-            Console.WriteLine(a);
-            // Console.WriteLine(retorno);
+        //    Console.WriteLine(a);
+        //    // Console.WriteLine(retorno);
 
-            classeteste obj = new classeteste();
-            obj.nome = "teste";
+        //    classeteste obj = new classeteste();
+        //    obj.nome = "teste";
 
-            Console.WriteLine(obj.nome);
-            Calculator.alteraNome(obj);
-            Console.WriteLine(obj.nome);
+        //    Console.WriteLine(obj.nome);
+        //    Calculator.alteraNome(obj);
+        //    Console.WriteLine(obj.nome);
 
-        }
+        //}
     }
 }
